455f4358d3b29d7f66c5b1e637d1da92fad93516
Seamus Martin <spmartin823@gmail.com>
2017-10-24 20:36:07 -0400

    commit 4.

--- src/App.js
+++ src/App.js
@@ -17,41 +17,37 @@ class App extends Component {
         };
     }
 
-    // var moveHandler = (move) => {
-    // // this takes the move from the state and
-    //
-    // };
-
-    boardStateHandler() {
-        // if the boardState is null, give the board the board skeleton.
-        if (!this.state.boardState) {
-             this.setState({
-                 // hist canMove
-                 board: {
-                     0: {hist : [], owns : null, canMove : []}, 
-                     1: {hist : [], owns : null, canMove : []},
-                     2: {hist : [], owns : null, canMove : []},
-                     3: {hist : [], owns : null, canMove : []},
-                     4: {hist : [], owns : null, canMove : []},
-                     5: {hist : [], owns : null, canMove : []},
-                     6: {hist : [], owns : null, canMove : []},
-                     7: {hist : [], owns : null, canMove : []},
-                     8: {hist : [], owns : null, canMove : []}
-                 },
-             })
-        } else {
-            // boardState is not null, need to check for new move.
-
-
-
-
-            var ownHandler = (board) => {
-                // this is a helper method that takes in the board object, reads the history
-
-            }
+    // this is only called once, before the initial render.
+    componentWillMount() {
+            this.setState({boardState: {
+                0: {hist : [], owns : null, canMove : []},
+                1: {hist : [], owns : null, canMove : []},
+                2: {hist : [], owns : null, canMove : []},
+                3: {hist : [], owns : null, canMove : []},
+                4: {hist : [], owns : null, canMove : []},
+                5: {hist : [], owns : null, canMove : []},
+                6: {hist : [], owns : null, canMove : []},
+                7: {hist : [], owns : null, canMove : []},
+                8: {hist : [], owns : null, canMove : []}
+            }})
         }
-
     }
+
+    moveHandler = (move) => {
+    // this takes the lastMove as an arguement from this.state and then updates the hist of the
+    // appropriate piece with the move.
+
+    };
+
+    ownHandler = (board) => {
+        // this is a helper method that takes in the board object, reads the history (which means that it
+        // needs to be changed with the move handler), and updates the own
+
+    };
+
+    boardStateHandler = () => {
+    };
+
 
   render() {
     return (
@@ -60,7 +56,6 @@ class App extends Component {
             <GameBoard
                 winStatus={this.state.winStatus}
                 boardState={this.state.boardState}
-                lastMove={this.state.lastMove}
             />
           </div>
           <div>
